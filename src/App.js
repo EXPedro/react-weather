dc4e79b436d32f7127c42f05f79f307b786e8bd1
EXPedro <pedroelisio555@gmail.com>
2022-03-30 08:56:34 -0300

    Add back image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Container from './components/container';
 
 function App() {
   return (
     <div className="App">
       Hello!
+      <Container />
     </div>
   );
 }
---
+++ src/components/container/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './container.css'
+
+function Container(){
+    return(
+        <div className='container'>
+            Container
+        </div>
+    )
+};
+
+export default Container;
