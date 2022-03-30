c35f84322391fe9248ff96cbeddc1ce9f5477a0e
EXPedro <pedroelisio555@gmail.com>
2022-03-30 10:38:58 -0300

    Create datamodel

--- src/components/datashow/index.jsx
+++ src/components/datashow/index.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import './datashow.css'
+import './datashow.css';
 
 function Datashow(){
     return(
         <div className='datashow'>
-            datashow
+            <span>Curitiba</span>
+            <h1>20 °C</h1>
+            <span>icon</span>
+            <span>Descrição</span>
+            <span>Sensação Térmica</span>
+            <span>Mìn.</span>
+            <span>Max.</span>
+            <span>Pressão</span>
+            <span>Umidade</span>
+            <span>Vento (m/s)</span>
+            <span>Nascer do Sol</span>
+            <span>Pôr do Sol</span>
         </div>
     )
 };
