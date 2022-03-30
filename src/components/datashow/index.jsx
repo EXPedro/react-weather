2aced347396b6a69e3c58713bc4da78fb906c399
EXPedro <pedroelisio555@gmail.com>
2022-03-30 10:56:41 -0300

    Create grid layout

--- src/components/datashow/index.jsx
+++ src/components/datashow/index.jsx
@@ -4,18 +4,18 @@ import './datashow.css';
 function Datashow(){
     return(
         <div className='datashow'>
-            <span>Curitiba</span>
-            <h1>20 °C</h1>
-            <span>icon</span>
-            <span>Descrição</span>
-            <span>Sensação Térmica</span>
-            <span>Mìn.</span>
-            <span>Max.</span>
-            <span>Pressão</span>
-            <span>Umidade</span>
-            <span>Vento (m/s)</span>
-            <span>Nascer do Sol</span>
-            <span>Pôr do Sol</span>
+            <span className='cidade'>Curitiba</span>
+            <h1 className='temperatura'>20 °C</h1>
+            <span className='icone'>icon</span>
+            <span className='descricao'>Descrição</span>
+            <span className='sensacao'>Sensação Térmica</span>
+            <span className='min'>Mín.</span>
+            <span className='max'>Max.</span>
+            <span className='pressao'>Pressão</span>
+            <span className='umidade'>Umidade</span>
+            <span className='vento'>Vento (m/s)</span>
+            <span className='nascer'>Nascer do Sol</span>
+            <span className='por'>Pôr do Sol</span>
         </div>
     )
 };
