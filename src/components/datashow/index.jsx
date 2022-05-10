d1a89889856d018251c8990e7150477883b80870
EXPedro <pedroelisio555@gmail.com>
2022-05-10 11:18:16 -0300

    Update datashow

--- src/components/datashow/index.jsx
+++ src/components/datashow/index.jsx
@@ -1,35 +1,79 @@
 import React from "react";
 import './datashow.css';
-import Weather from '../../data/weather.json';
-//import {Weather} from '../../providers/index.js';
-//import { resposta } from "../../services";
+import { useState, useEffect } from 'react';
+import { resposta } from "../../services";
+//import Weather from '../../data/weather.json';
+//import Weather from '../../providers/index.jsx';
+//import { resposta } from "../../services";  src\providers\index.js
 import {numeroComVirgula, converteUnixTime} from '../../utils/utils.js';
 
-function Datashow(){
+const Datashow = () => {
+
+    const [clima, setClima] = useState({
+        coord:{
+            lon: 0,
+            lat: 0
+            },
+        weather:[{
+            id:803,
+            main:"Clouds",
+            description:"nublado",
+            icon:"04d"
+            }],
+        base:"stations",
+        main:{
+            temp:20.19,
+            feelslike:20.4,
+            tempmin:18.95,
+            temp_max:21.16,
+            pressure:1020,
+            humidity:83
+            },
+        visibility:10000,
+        wind:{
+            speed:2.57,
+            deg:50
+            },
+        clouds:{
+            all:75
+            },
+        dt:1648558182,
+        sys:{
+            type:2,
+            id:67576,
+            country:"BR",
+            sunrise:1648545878,
+            sunset:1648588738
+            },
+        timezone:-10800,
+        id:6322752,
+        name:"Curitiba",
+        cod:200
+    });
 
     //const Weather = resposta;
     //Substitui ponto por vírgula nas variáveis
-    const temperatura = numeroComVirgula(Weather.main.temp);
-    const sensacao = numeroComVirgula(Weather.main.feelslike);
-    const min = numeroComVirgula(Weather.main.tempmin);
-    const max = numeroComVirgula(Weather.main.temp_max);
-    const vento = numeroComVirgula(Weather.wind.speed);
+    const temperatura = numeroComVirgula(clima.main.temp);
+    const sensacao = numeroComVirgula(clima.main.feelslike);
+    const min = numeroComVirgula(clima.main.tempmin);
+    const max = numeroComVirgula(clima.main.temp_max);
+    const vento = numeroComVirgula(clima.wind.speed);
 
     //Converte Tempo
-    const nascer = converteUnixTime(Weather.sys.sunrise);
-    const por = converteUnixTime(Weather.sys.sunset);
+    const nascer = converteUnixTime(clima.sys.sunrise);
+    const por = converteUnixTime(clima.sys.sunset);
 
     return(
         <div className='datashow'>
-            <span className='cidade'>{Weather.name}</span>
+            <span className='cidade'>{clima.name}</span>
             <h1 className='temperatura'>{temperatura}°C</h1>
-            <span className='icone'>.{Weather.weather[0].icon}</span>
-            <span className='descricao'>{Weather.weather[0].description}</span>
+            <span className='icone'>.{clima.weather[0].icon}</span>
+            <span className='descricao'>{clima.weather[0].description}</span>
             <span className='sensacao'>Sensação Térmica: {sensacao}°C</span>
             <span className='min'>Mín: {min}°C</span>
             <span className='max'>Máx: {max}°C</span>
-            <span className='pressao'>Pressão: {Weather.main.pressure}hPa</span>
-            <span className='umidade'>Umidade: {Weather.main.humidity}%</span>
+            <span className='pressao'>Pressão: {clima.main.pressure}hPa</span>
+            <span className='umidade'>Umidade: {clima.main.humidity}%</span>
             <span className='vento'>Vento: {vento}m/s</span>
             <span className='nascer'>Nascer do Sol: {nascer}</span>
             <span className='por'>Pôr do Sol: {por}</span>  
--- src/providers/index.js
+++
@@ -1,9 +0,0 @@
-import { resposta } from "../services";
-
-export function Weather() {    
-    return resposta;
-};
-
-
-
-
